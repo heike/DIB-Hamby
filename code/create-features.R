74f7cc10b4c8b7ad9c5c565373b659d27ca345ad
heike <hofmann@iastate.edu>
2020-12-22 17:16:37 -0600

    correct code

--- code/create-features.R
+++ code/create-features.R
@@ -1,153 +1,151 @@
 library(tidyverse)
 library(x3ptools)
-library(bulletxtrctr)
-x3p <- read_x3p("~/papers/dissertations/eric-dissertation/images/Hamby (2009) Barrel/bullets/Barrel 1/Br1 Bullet 1-5.x3p")
-x3p <- x3p %>% x3p_rotate(angle = -90)
-x3p <- x3p %>% y_flip_x3p()
-x3p <- x3p %>% x3p_m_to_mum()
-#x3p %>% x3p_image()
-
-cc <- x3p %>% x3p_crosscut_optimize()
-ccdata <- x3p %>% x3p_crosscut(y = cc)
-ccdata %>% ggplot(aes(x = x, y = value)) + geom_line()
-
-grooves <- ccdata %>% cc_locate_grooves(return_plot = TRUE)
-
-sigs <- ccdata %>% cc_get_signature(grooves)
-sigs %>%
-  ggplot(aes(x = x, y = sig)) + geom_line()
-
-
-bstats <- read.csv("~/papers/dissertations/eric-dissertation/data/data-25-25/bullet-stats.csv", stringsAsFactors = FALSE)
-
-bullets <- read_bullet("~/papers/dissertations/eric-dissertation/images/Hamby (2009) Barrel/bullets/Barrel 1/")
-bullets <- bullets %>%
+library(bulletxtrctr) # remotes::install_github("heike/bulletxtrctr")
+b252 <- read_bullet("NBTRD/Hamby 252 (2009) Barrel/bullets/")
+b252 <- b252 %>%
   mutate(
     x3p = x3p %>% purrr::map(.f = function(x) {
       x <- x %>% x3p_rotate(angle=-90) %>% y_flip_x3p()
       x %>% x3p_m_to_mum()
     })
   )
-
-bullets <- bullets %>% mutate(
-  cc = x3p %>% purrr::map(.f = function(x) x3p_crosscut_optimize(x))
+b252 <- b252 %>% mutate(
+  study = "Hamby252",
+  barrel = ifelse(grepl("Ukn", source), "Ukn", gsub(".*/Br([0-9]+).*","\\1", source)),
+  bullet = gsub(".*Bullet ([A-Z12]).*", "\\1", source),
+  land = gsub(".*Bullet [12A-Z]-([1-6]).*", "\\1", source),
+  land_id = sprintf("%s-Br%s-B%s-L%s",study, barrel, bullet, land)
 )
 
-bullets <- bullets %>% mutate(
-  ccdata = purrr::map2(.x = x3p, .y = cc, .f = function(x, y) x3p_crosscut(x3p=x, y = y))
-)
-
-bullets <- bullets %>% mutate(
-  grooves = ccdata %>% purrr::map(.f = function(x) cc_locate_grooves(x))
-)
-
-bullets <- bullets %>% mutate(
-  sigs = purrr::map2(.x = ccdata, .y = grooves, .f = function(x, y) cc_get_signature(ccdata=x, grooves = y))
-)
-
-signatures <- bullets %>% unnest(sigs)
-signatures %>%
-  ggplot(aes( x= x, y = sig)) +
-  geom_line() +
-  facet_wrap(~source, ncol=6)
-
-
-lands <- unique(bullets$source)
-comparisons <- data.frame(
-  expand.grid(land1 = lands, land2 = lands), stringsAsFactors = FALSE)
-
-comparisons <- comparisons %>% mutate(
-  aligned = purrr::map2(.x = land1, .y = land2, .f = function(xx, yy) {
-    land1 <- bullets$sigs[bullets$source == xx][[1]]
-    land2 <- bullets$sigs[bullets$source == yy][[1]]
-    land1$bullet <- "first-land"
-    land2$bullet <- "second-land"
-
-    sig_align(land1$sig, land2$sig)
-  })
-)
-
-comparisons <- comparisons %>% mutate(
-  striae = aligned %>% purrr::map(.f = sig_cms_max, span = 75)
-)
-
-comparisons <- comparisons %>% mutate(
-  legacy_features = purrr::map(striae, extract_features_all_legacy, resolution = 1.5625)
-)
-
-legacy <- comparisons %>% tidyr::unnest(legacy_features)
-legacy <- legacy %>% mutate(
-  bullet1 = gsub(".*(Bullet [12]).*", "\\1", land1),
-  l1 = gsub(".*Bullet [12]-([1-6]).*", "\\1", land1),
-  land_id1 = sprintf("Hamby252-Br1-B%d-L%s", parse_number(bullet1), l1)
-)
-legacy <- legacy %>% mutate(
-  bullet2 = gsub(".*(Bullet [12]).*", "\\1", land2),
-  l2 = gsub(".*Bullet [12]-([1-6]).*", "\\1", land2),
-  land_id2 = sprintf("Hamby252-Br1-B%d-L%s", parse_number(bullet2), l2)
-)
-
-legacy %>% ggplot(aes(x = land_id1, y=land_id2, fill=ccf)) + geom_tile() +
-  scale_fill_gradient2(low="darkgrey", mid="white", high = "darkorange", midpoint = 0.5)
-
-
-cf <- read.csv("data/hamby-comparisons.csv")
-br1 <- cf %>% filter(grepl("Hamby252-Br1-", land_id1), grepl("Hamby252-Br1-", land_id2))
-br1 %>% ggplot(aes(x = land_id1, y=land_id2, fill=ccf)) + geom_tile() +
-  scale_fill_gradient2(low="darkgrey", mid="white", high = "darkorange", midpoint = 0.5)
-
-
-full_features <- legacy %>%
-  left_join(br1, by=c("land_id1", "land_id2"))
-
-feature_x <- full_features %>% select(land_id1, land_id2, ends_with(".x")) %>%
-  pivot_longer(ends_with(".x"), names_to = "feature", values_to="values") %>%
-  mutate(
-    feature = gsub(".x", "", feature)
-  )
-feature_y <- full_features %>% select(land_id1, land_id2, ends_with(".y")) %>%
-  pivot_longer(ends_with(".y"), names_to = "feature", values_to="values") %>%
-  mutate(
-    feature = gsub(".y", "", feature)
-  )
-
-features <- feature_x %>% left_join(feature_y, by=c("land_id1", "land_id2", "feature"))
-
-features %>%
-  ggplot(aes(x = values.x, y = values.y)) + geom_point() +
-  facet_wrap(~feature, scales="free")
-
-####
-# try all barrels
-
-bullets <- read_bullet("~/papers/dissertations/eric-dissertation/images/Hamby (2009) Barrel/bullets/")
-bullets <- bullets %>%
+b173 <- read_bullet("NBTRD/Hamby 173 (2009) Barrel/bullets/")
+b173 <- b173 %>%
   mutate(
     x3p = x3p %>% purrr::map(.f = function(x) {
-    #  browser()
-      dims <- dim(x$surface.matrix)
-      if (dims[1] < dims[2]) {
-        x <- x %>% x3p_rotate(angle=-90) %>% y_flip_x3p()
-      } else {
-         x <- x %>% y_flip_x3p()
-      }
+      x <- x %>% y_flip_x3p()
       x %>% x3p_m_to_mum()
     })
   )
+b173 <- b173 %>% mutate(
+  study = "Hamby173",
+  barrel = gsub(".*/br([A-Z0-9]+)_.*", "\\1", source),
+  bullet = gsub(".*_([12])_.*", "\\1", source),
+  land = gsub(".*_land([1-6]).*", "\\1", source)
+)
+# warning is expected - 90 lands from unknown barrels
+b173 <- b173 %>% mutate(
+  bullet = ifelse(is.na(parse_number(barrel)), barrel, bullet),
+  barrel = ifelse(is.na(parse_number(barrel)), "Ukn", barrel)
+)
+b173 <- b173 %>% mutate(
+  land_id = sprintf("%s-Br%s-B%s-L%s",study, barrel, bullet, land)
+)
 
-cc <- rep(NA, nrow(bullets))
+bullets <- rbind(b173, b252)
+#write.csv(bullets %>% select(-x3p), "meta-info.csv", row.names = FALSE)
+
+bullets <- bullets %>% mutate(
+  cc = x3p %>% purrr::map_dbl(.f = function(x) x3p_crosscut_optimize(x))
+)
+
+##########
+# check crosscuts
 for (i in 1:nrow(bullets)) {
-  cc[i] <- bullets$x3p[[i]] %>% x3p_crosscut_optimize()
+  x3p <- bullets$x3p[[i]]
+  x3p <- x3p %>% x3p_add_hline(yintercept = bullets$cc[[i]], size = 10, color = "white")
+  x3p %>% image_x3p(file=paste0("images/",bullets$land_id[[i]],".png"))
 }
-bullets$cc <- cc
+# identify problematic crosscuts
+cc_manual <- dir("images/crosscut/")
+cc_manual <- gsub(".png", "", cc_manual)
+bullets$cc[which(bullets$land_id %in% cc_manual)]
+bullets$cc[which(bullets$land_id %in% cc_manual)] <- c(4,3)*bullets$cc[which(bullets$land_id %in% cc_manual)]
+for (i in which(bullets$land_id %in% cc_manual)) {
+  x3p <- bullets$x3p[[i]]
+  x3p <- x3p %>% x3p_add_hline(yintercept = bullets$cc[[i]], size = 10, color = "white")
+  x3p %>% image_x3p(file=paste0("images/crosscut/",bullets$land_id[[i]],"-manual.png"))
+}
 
+
+##########
 bullets <- bullets %>% mutate(
   ccdata = purrr::map2(.x = x3p, .y = cc, .f = function(x, y) x3p_crosscut(x3p=x, y = y))
 )
 
 bullets <- bullets %>% mutate(
-  grooves = ccdata %>% purrr::map(.f = function(x) cc_locate_grooves(x))
+  grooves = ccdata %>% purrr::map(.f = function(x) cc_locate_grooves(x, return_plot = TRUE))
 )
+
+###########
+library(shiny)
+grooves <- bullets$grooves
+
+shinyApp(
+  ui = fluidPage(
+    selectInput("k","Investigate kth plot:", selected = 1,
+                choices=1:length(grooves)),
+    textOutput("groovelocations"),
+    actionButton("confirm", "Confirm"),
+    actionButton("save", "Save"),
+    plotOutput("groovePlot", click = "plot_click"),
+    verbatimTextOutput("info")
+  ),
+
+  server = function(input, output, session) {
+    output$groovePlot <- renderPlot({
+      k <- as.numeric(input$k)
+      p <- grooves[[k]]$plot
+
+      p
+    })
+    output$groovelocations <- renderText({
+      paste("Left Groove: ",grooves[[as.numeric(input$k)]]$groove[1],
+            " Right Groove: ",grooves[[as.numeric(input$k)]]$groove[2])
+    })
+    observeEvent(input$confirm,{
+      cat(str(input$k))
+      updateSelectInput(session, "k","Investigate kth plot:",
+                        selected = as.numeric(input$k)+1,
+                        choices=1:length(grooves))
+    })
+    observeEvent(input$save,{
+      saveRDS(grooves, file="data/grooves.rda")
+      cat("groove data saved\n")
+    })
+
+    observeEvent(input$plot_click,{
+      k <- as.numeric(input$k)
+      xloc <- input$plot_click$x
+
+      gr <- grooves[[k]]$groove
+      if (abs(gr[1]-xloc) < abs(gr[2]-xloc)) {
+        grooves[[k]]$groove[1] <<- xloc
+      } else {
+        grooves[[k]]$groove[2] <<- xloc
+      }
+      output$groovePlot <- renderPlot({
+        k <- as.numeric(input$k)
+        p <- grooves[[k]]$plot +
+          geom_vline(xintercept = grooves[[k]]$groove[1], colour="green") +
+          geom_vline(xintercept = grooves[[k]]$groove[2], colour="green")
+
+        p
+      })
+
+    })
+    output$info <- renderText({
+      paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
+    })
+
+  },
+
+  options = list(height = 500)
+)
+
+bullets$grooves_pred <- bullets$grooves
+bullets$grooves <- grooves
+
+###########
+
 
 bullets <- bullets %>% mutate(
   sigs = purrr::map2(.x = ccdata, .y = grooves, .f = function(x, y) cc_get_signature(ccdata=x, grooves = y))
@@ -155,115 +153,186 @@ bullets <- bullets %>% mutate(
 
 signatures <- bullets %>% unnest(sigs)
 signatures %>%
+  filter(study=="Hamby252", barrel %in% c(1:5)) %>%
   ggplot(aes( x= x, y = sig)) +
-  geom_line() +
-  facet_wrap(~source, ncol=6)
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H252-br1-5.png", width=9, height=8)
 
-saveRDS(bullets, "bullets.rds")
+signatures %>%
+  filter(study=="Hamby173", barrel %in% c(1:5)) %>%
+  ggplot(aes( x= x, y = sig)) +
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H173-br1-5.png", width=9, height=8)
 
-lands <- unique(bullets$source)
+signatures %>%
+  filter(study=="Hamby252", barrel %in% c(6:10)) %>%
+  ggplot(aes( x= x, y = sig)) +
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H252-br6-10.png", width=9, height=8)
+
+signatures %>%
+  filter(study=="Hamby173", barrel %in% c(6:10)) %>%
+  ggplot(aes( x= x, y = sig)) +
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H173-br6-10.png", width=9, height=8)
+
+
+signatures %>%
+  filter(study=="Hamby252", barrel == "Ukn") %>%
+  ggplot(aes( x= x, y = sig)) +
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H252-br-unk.png", width=9, height=10)
+
+signatures %>%
+  filter(study=="Hamby173", barrel == "Ukn") %>%
+  ggplot(aes( x= x, y = sig)) +
+  geom_line(aes(colour = damaged)) +
+  facet_grid(barrel+bullet~study+land, labeller="label_both") +
+  ylim(c(-10,10)) +
+  scale_colour_manual(values=c("grey30", "darkred")) +
+  theme_bw()
+ggsave(file="H173-br-unk.png", width=9, height=10)
+
+
+
+lands <- unique(bullets$land_id)
 comparisons <- data.frame(
   expand.grid(land1 = lands, land2 = lands), stringsAsFactors = FALSE)
 
-comparisons <- comparisons %>% mutate(
-  aligned = purrr::map2(.x = land1, .y = land2, .f = function(xx, yy) {
-    land1 <- bullets$sigs[bullets$source == xx][[1]]
-    land2 <- bullets$sigs[bullets$source == yy][[1]]
-    land1$bullet <- "first-land"
-    land2$bullet <- "second-land"
 
-    sig_align(land1$sig, land2$sig)
-  })
+alignHelper <- function(xx, yy) {
+  land1 <- bullets$sigs[bullets$land_id == xx][[1]]
+  land2 <- bullets$sigs[bullets$land_id == yy][[1]]
+  land1$bullet <- "first-land"
+  land2$bullet <- "second-land"
+
+  sig_align(land1$sig, land2$sig)
+}
+# # sequential version - use parallelized version below
+# comparisons <- comparisons %>% mutate(
+#   aligned = purrr::map2(.x = land1, .y = land2, .f = alignHelper)
+# )
+
+library(parallel)
+numCores <- detectCores()
+comp_i <- function(i) {
+  cc <- comparisons[i,]
+  alignHelper(cc$land1, cc$land2)
+}
+
+system.time(
+  aligned <- mclapply(1:nrow(comparisons), FUN = comp_i, mc.cores = numCores-1)
 )
+
+#      user    system   elapsed
+# 42227.436  6808.655  4891.661
+
+
+# sequential, parallel re-write below
+# comparisons <- comparisons %>% mutate(
+#   striae = aligned %>% purrr::map(.f = sig_cms_max, span = 75)
+# )
+
+striae_i <- function(i) {
+  sig_cms_max(aligned[[i]], span=75)
+}
+
+system.time(
+  striae <- mclapply(1:nrow(comparisons), FUN = striae_i, mc.cores = numCores-1)
+)
+
+#      user    system   elapsed
+# 37560.232  7273.051  4639.551
+
+comparisons <- tibble(comparisons, aligned, striae)
 saveRDS(comparisons, "comparisons.rds")
 
-comparisons <- comparisons %>% mutate(
-  striae = aligned %>% purrr::map(.f = sig_cms_max, span = 75)
-)
 
-comparisons <- comparisons %>% mutate(
-  legacy_features = purrr::map(striae, extract_features_all_legacy, resolution = 1.5625)
+# comparisons <- comparisons %>% mutate(
+#   legacy_features = purrr::map(striae, extract_features_all_legacy, resolution = 1.5625)
+# )
+
+legacy_i <- function(i) {
+  extract_features_all_legacy(striae[[i]], resolution = 1.5625)
+}
+
+system.time(
+  legacy_features <- mclapply(1:nrow(comparisons), FUN = legacy_i, mc.cores = numCores-1)
 )
+#     user   system  elapsed
+# 9445.579  392.776  754.253
+comparisons <- tibble(comparisons, legacy_features)
 saveRDS(comparisons, "comparisons.rds")
 
-legacy <- comparisons %>% tidyr::unnest(legacy_features)
-legacy <- legacy %>% mutate(
-  study1 = ifelse(grepl("/Br", legacy$land1), "Hamby252", NA),
-  study1 = ifelse(grepl("/Ukn", legacy$land1), "Hamby252", study1),
-  study1 = ifelse(grepl("/br", legacy$land1), "Hamby173", study1),
-  study2 = ifelse(grepl("/Br", legacy$land2), "Hamby252", NA),
-  study2 = ifelse(grepl("/Ukn", legacy$land2), "Hamby252", study2),
-  study2 = ifelse(grepl("/br", legacy$land2), "Hamby173", study2)
+# comparisons <- comparisons %>% mutate(
+#   legacy_features = purrr::map(striae, extract_features_all_legacy, resolution = 1.5625)
+# )
+
+feature_i <- function(i) {
+  extract_features_all(aligned[[i]], striae[[i]], resolution = 1.5625)
+}
+
+system.time(
+  features <- mclapply(1:nrow(comparisons), FUN = feature_i, mc.cores = numCores-1)
 )
+#  user   system  elapsed
+# 3.220    9.590 1426.357
+comparisons <- tibble(comparisons, features)
+saveRDS(comparisons, "comparisons.rds")
 
-legacy <- legacy %>% mutate(
-  barrel1 = gsub(".*((Br[0-9]+)|(Ukn)|(br[0-9A-Z]+)).*", "\\1", land1),
-  barrel1 = ifelse(is.na(parse_number(barrel1)), "Ukn", parse_number(barrel1))
-)
-legacy <- legacy %>% mutate(
-  barrel2 = gsub(".*((Br[0-9]+)|(Ukn)|(br[0-9A-Z]+)).*", "\\1", land2),
-  barrel2 = ifelse(is.na(parse_number(barrel2)), "Ukn", parse_number(barrel2))
-)
+##########
+# include ground truth
+meta <- read.csv("meta-info.csv", stringsAsFactors = FALSE)
 
-legacy <- legacy %>% mutate(
-  bullet1 = gsub(".*((Bullet [12A-Z])|(_[12]_)).*", "\\1", land1),
-  bullet1 = gsub("Bullet ", "", bullet1),
-  bullet1 = ifelse(is.na(parse_number(bullet1)), bullet1, parse_number(bullet1))
-)
-legacy <- legacy %>% mutate(
-  bullet2 = gsub(".*((Bullet [12A-Z])|(_[12]_)).*", "\\1", land2),
-  bullet2 = gsub("Bullet ", "", bullet2),
-  bullet2 = ifelse(is.na(parse_number(bullet2)), bullet2, parse_number(bullet2))
-)
+comparisons <- comparisons %>%
+  left_join(meta %>% select(land_id, index_land1=H173_B1_index), by=c("land1"="land_id")) %>%
+  left_join(meta %>% select(land_id, index_land2=H173_B1_index), by=c("land2"="land_id"))
 
-legacy <- legacy %>% mutate(
-  l1 = gsub(".*Bullet [12A-Z]-([1-6]).*", "\\1", land1),
-  l1 = gsub(".*_land([1-6]).*", "\\1", l1),
-)
-legacy <- legacy %>% mutate(
-  l2 = gsub(".*Bullet [12A-Z]-([1-6]).*", "\\1", land2),
-  l2 = gsub(".*_land([1-6]).*", "\\1", l2),
-)
+comparisons$samesource <- comparisons$index_land1 == comparisons$index_land2
+saveRDS(comparisons, "comparisons.rds")
 
+##########
+# mark comparisons from damaged LEAs
 
-legacy <- legacy %>% mutate(
-  land_id1 = sprintf("%s-Br%s-B%s-L%s", study1, barrel1, bullet1, l1),
-  land_id2 = sprintf("%s-Br%s-B%s-L%s", study2, barrel2, bullet2, l2)
-)
+comparisons <- comparisons %>%
+  left_join(meta %>% select(land_id, flagged1=damaged), by=c("land1"="land_id")) %>%
+  left_join(meta %>% select(land_id, flagged2=damaged), by=c("land2"="land_id"))
+comparisons$damaged <- comparisons$flagged1|comparisons$flagged2
+comparisons %>% count(damaged)
 
+##########
+# use only one comparison of a pair of scans
 
-write.csv(legacy %>% select(-aligned, -striae), "Hamby173-252-features.csv", row.names=FALSE)
-
-
-legacy %>% filter(study1 == "Hamby252", study2 == "Hamby252") %>%
-  ggplot(aes(x = l1, y=l2, fill=ccf)) + geom_tile() +
-  scale_fill_gradient2(low="darkgrey", mid="white", high = "darkorange", midpoint = 0.5) +
-  facet_grid(barrel1+bullet1~barrel2+bullet2)
-
-
-cf <- read.csv("data/hamby-comparisons.csv")
-
-
-full_features <- legacy %>%
-  left_join(cf, by=c("land_id1", "land_id2"))
-
-feature_x <- full_features %>% select(land_id1, land_id2, ends_with(".x")) %>%
-  pivot_longer(ends_with(".x"), names_to = "feature", values_to="values") %>%
+comparisons <- comparisons %>%
   mutate(
-    feature = gsub(".x", "", feature)
+    id = paste(pmin(land1, land2), pmax(land1,land2), sep="|")
   )
-feature_y <- full_features %>% select(land_id1, land_id2, ends_with(".y")) %>%
-  pivot_longer(ends_with(".y"), names_to = "feature", values_to="values") %>%
-  mutate(
-    feature = gsub(".y", "", feature)
-  )
+saveRDS(comparisons, "comparisons.rds")
 
-feature_y <- na.omit(feature_y)
-features <- feature_x %>% left_join(feature_y, by=c("land_id1", "land_id2", "feature"))
-features <- features %>% left_join(cf %>% select(land_id1, land_id2, same_source), by=c("land_id1", "land_id2"))
-
-features %>%
-  ggplot(aes(x = values.x, y = values.y, colour = same_source)) + geom_point() +
-  facet_wrap(~feature, scales="free")
+features <- comparisons %>% select(-ccf) %>% unnest(features)
+doubles <- duplicated(features$id)
+features <- features %>% filter(!doubles, !damaged)
+features <- features %>% mutate(abs_lag_mm = abs(lag_mm))
+write.csv(features %>% select(-aligned,-striae, -legacy_features), "training-features.csv", row.names = FALSE)
 
 
