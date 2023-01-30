27e751bf0f0e7571f99434d3ab4e7e009b1ac027
Heike Hofmann <hofmann@iastate.edu>
2023-01-30 11:54:24 -0600

    fitting csafe_rf3

--- code/fitting-randomforest.R
+++ code/fitting-randomforest.R
@@ -7,26 +7,22 @@ library(randomForest)
 
 # train random forest
 set.seed(20140105)
-csafe_rf3 <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
+csafe_rf3_big <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
                             mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
-                          importance=TRUE, ntree=200,
-                          data = features)
-
-csafe_rf3 <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
-                          mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
                           importance=TRUE, ntree=1000,
                           data = features)
-plot(csafe_rf4)
+
+plot(csafe_rf3_big)
 abline(a=0.09, b=0)
 which.min(csafe_rf4$err.rate[,1])
 csafe_rf4$err.rate[196,]
 csafe_rf4$err.rate[200,]
 
 set.seed(20140105)
-csafe_rf3sub <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
+csafe_rf3 <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
                             mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
-                          importance=TRUE, ntree=331,
+                          importance=TRUE, ntree=200,
                           data = features)
 
 
-saveRDS(csafe_rf3sub, "data/csafe_rf3.rds")
+saveRDS(csafe_rf3, "data/csafe_rf3.rds")
