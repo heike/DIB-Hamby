436c9f4749cfc5c28d0c3c25bc14932d3830a4bd
nathanrethwisch <nreth@iastate.edu>
2025-02-19 13:37:16 -0600

    Updating code for fitting-randomforest to take out csafe_rf3_big

--- code/fitting-randomforest.R
+++ code/fitting-randomforest.R
@@ -6,23 +6,21 @@ features <- read.csv("data/hamby-comparisons.csv")
 library(randomForest)
 
 # train random forest
-set.seed(20140105)
-csafe_rf3_big <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
-                            mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
-                          importance=TRUE, ntree=1000,
-                          data = features)
+#set.seed(20140105)
+#csafe_rf3_big <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
+#                            mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
+#                          importance=TRUE, ntree=1000,
+#                          data = features)
+#
+#plot(csafe_rf3_big)
+#abline(a=0.09, b=0)
 
-plot(csafe_rf3_big)
-abline(a=0.09, b=0)
-which.min(csafe_rf4$err.rate[,1])
-csafe_rf4$err.rate[196,]
-csafe_rf4$err.rate[200,]
+
 
 set.seed(20140105)
 csafe_rf3 <- randomForest(factor(samesource) ~ ccf + rough_cor + matches_per_mm + D + sd_D+ cms_per_mm +
                             mismatches_per_mm + overlap + abs_lag_mm + sum_peaks,
                           importance=TRUE, ntree=200,
                           data = features)
-
 
 saveRDS(csafe_rf3, "data/csafe_rf3.rds")
